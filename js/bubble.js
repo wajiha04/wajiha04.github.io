ccd5fb6522446183d04750006354fdaa10be4609
wajiha04 <>
2021-03-22 12:53:15 -0700

    add changes

---
+++ js/bubble.js
@@ -0,0 +1,18 @@
+@keyframe bubble1 {
+  0%{
+    transform: translateY(0);
+    opacity: 0;
+  }
+  50%{
+    /* transform: translateY(0); */
+    opacity:1;
+  }
+  70%{
+    /* transform: translateY(0); */
+    opacity: 1;
+  }
+  100%{
+    transform: translateY(-80vh);
+    opacity: 0;
+  }
+}
